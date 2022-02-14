916c18ea9b3477c60181669bf7e211d16e374ed6
TheSupremeToast <patrickdewey2002@gmail.com>
2022-02-14 00:13:25 -0500

    updated pi_spot.py

--- pi_spot.py
+++ pi_spot.py
@@ -2,37 +2,63 @@ import os
 import time
 import spotipy
 
+##################
+### pi_spot.py ###
+##################
+
+##################
+
+# Server to server requests - doesn't require user auth
+
+# Set up client credential flow for non-user information requests
 from spotipy.oauth2 import SpotifyClientCredentials
-# Allow user to grant permissions only once
-from spotipy.oauth2 import SpotifyOAuth
 
 from dotenv import load_dotenv
 
 # get token as environment variable
+# application authentication variables
 load_dotenv()
 client = os.getenv('SPOTIFY_CLIENT_ID')
 secret = os.getenv('SPOTIFY_CLIENT_SECRET')
 uri = os.getenv('SPOTIFY_REDIRECT_URI')
 
-# prompt login?
-# spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+client_auth = SpotifyClientCredentials()
+client = spotipy.Spotify(auth_manager = client_auth)
 
-# Set scope to target user library
-# scope = 'user-library-read'
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id = client, 
-    client_secret = secret, redirect_uri = uri))
+##################
+
+# user to server requests - requires oauth
+
+# Allow user to grant permissions only once
+from spotipy.oauth2 import SpotifyOAuth
+
+# Authenticate to access individual user data
+scope = 'user-library-read'
+# user = spotipy.Spotify(auth_manager = SpotifyOAuth(scope = scope))
+
+##################
 
 # get user's top tracks
 # - limit: number of entries to return
 # - offset: first entity to return
 # - time_range = short_term, medium_term, long_term
-# results = sp.current_user_top_tracks()
+scope = 'user-top-read'
+user = spotipy.Spotify(auth_manager = SpotifyOAuth(scope = scope))
+results = user.current_user_top_tracks()
 
 # get user's top artists
 # - limit: number of entries to return
 # - offset: first entity to return
 # - time_range = short_term, medium_term, long_term
-sp.current_user_top_artists(25, time_range = 'short_term')
+# user.current_user_top_artists(25, time_range = 'short_term')
 
 # get user's playlists
-# results = sp.current_user_playlists()
+# results = user.current_user_playlists()
+
+print(results)
+
+
+
+##################
+
+# ENDFILE
